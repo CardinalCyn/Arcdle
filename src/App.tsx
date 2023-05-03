a34024134fd71998e7c6aa7270a64ff8544ae0dd
Salah Zanabili <Zanabilis@gmail.com>
2023-05-03 17:03:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import './App.css'
+import InputForm from './Components/InputForm'
+import Attempts from './Components/Attempts'
+import { SpellType } from './types/SpellType'
+import {useState,useEffect} from 'react'
+import { default as Spells } from './spells/spells.json'
+
+const App=()=> {
+  
+  const [correctSpell,setCorrectSpell]=useState<SpellType>(Spells[0]);
+
+  const generateRandomSpell=(date:Date)=>{
+    const seed= date.getTime();
+    const randomIndex= Math.floor(Math.sin(seed) * Spells.length);
+    return Spells[randomIndex];
+  }
+
+  useEffect(() => {
+    const today= new Date();
+    const randomSpell=generateRandomSpell(today);
+    setCorrectSpell(randomSpell);
+  }, []);
+
+  const [submissions,setSubmissions]=useState<SpellType[]>([]);
+  const [guessedCorrectly,setGuessedCorrectly]=useState(false);
+
+  const submitGuess=(guessValue:SpellType)=>{
+    setSubmissions([guessValue,...submissions]);
+    let guessedCorrect=true;
+    for(const key in guessValue){
+      if(guessValue[key as keyof SpellType]!==correctSpell[key as keyof SpellType]){
+        guessedCorrect=false;
+      }
+    }
+    setGuessedCorrectly(guessedCorrect);
+  }
+
+  return (
+    <div className='arcdleContainer'>
+      <h1 className='text-center text-9xl my-20'>Arcdle</h1>
+      <h2 className='text-center text-6xl'>Guess a spell!</h2>
+      {!guessedCorrectly&&<InputForm submitGuess={submitGuess} spells={Spells}/>}
+      {guessedCorrectly&&<div className='text-center'>You solved it in {submissions.length} tries!</div>}
+      {submissions.length>0&&<Attempts submissions={submissions} correctSpell={correctSpell}/>}
+    </div>
+  )
+}
+
+export default App
